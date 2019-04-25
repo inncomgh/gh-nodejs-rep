7456b46bc551a39f80d4029e1e3a0ccdaa4a0f6c
Calles <JORGELUIS.CALLESMORENO@Honeywell.com>
2019-04-25 18:01:13 -0500

    p

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const express = require('express');
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 const hostname  = '127.0.0.1';
 const app = express();
 
