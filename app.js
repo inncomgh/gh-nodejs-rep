f987c30271aa2fcbc3d244993be4c6079e4005c4
Calles <JORGELUIS.CALLESMORENO@Honeywell.com>
2019-04-29 15:41:33 -0500

    w3schools

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const http = require('http');
 const express = require('express');
 const app = express();
 
-http.createServer(function(req, res){
-		res.writeHead(200, {'Content-Type':'text/plain'});
-		res.end('Hello World!');	
-}).listen(process.env.PORT || 3000);
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World!');
+}).listen(process.env.PORT || 8080);
 
+
+console.log(process.env.PORT);
 
 /* const server = http.createServer((req, res) => {
 	res.statusCode = 200;
