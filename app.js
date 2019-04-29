0da862c9f07066f53fdbafc186056d0f50d8d935
Calles <JORGELUIS.CALLESMORENO@Honeywell.com>
2019-04-29 15:28:17 -0500

    w3schools

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 http.createServer(function(req, res){
 		res.writeHead(200, {'Content-Type':'text/plain'});
 		res.end('Hello World!');	
-}).listen(3000);
+}).listen(process.env.PORT || 3000);
 
 
 /* const server = http.createServer((req, res) => {
