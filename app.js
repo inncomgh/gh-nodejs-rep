d50b763e1c3c8b2d550577bab57729524ed1c3c4
Calles <JORGELUIS.CALLESMORENO@Honeywell.com>
2019-04-30 10:32:29 -0500

    added express approach with index

--- app.js
+++ app.js
@@ -1,24 +1,11 @@
-const http = require('http');
-const express = require('express');
-const app = express();
+const http = require('http')
+const express = require('express')
+const app = express()
 const PORT = process.env.PORT || 3000
+const path = require('path')
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!');
-}).listen(PORT, ()=> console.log(`Listening on ${ PORT }`));
-
-
-console.log(process.env.PORT);
-
-/* const server = http.createServer((req, res) => {
-	res.statusCode = 200;
-	res.setHeader('Content-Type', 'application/json');
-	res.end(JSON.stringify({
-		message: 'Response!!!'
-	}));
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
- */
-
 
 
